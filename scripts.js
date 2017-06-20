6b28174223bded76f6f453b990e07ba878cd9c8f
Edgar Juarez <ejjt.686@gmail.com>
2017-06-19 18:23:25 -0700

    New commit

--- scripts.js
+++ scripts.js
@@ -3,3 +3,13 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false}).then(localMediaStream => {
+        console.log(localMediaStream);
+        video.src = window.URL.createObjectURL(localMediaStream); // Converts media stream into something the video player can understand
+        video.play();
+    });
+}
+
+getVideo();
