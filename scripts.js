6a290bc4a2c6ed82c30b0517039d44d914ee9ed0
Edgar Juarez <ejjt.686@gmail.com>
2017-06-20 17:36:51 -0700

    Take Photo button is now linked to its function and screenshots appear under canvas for download

--- scripts.js
+++ scripts.js
@@ -33,11 +33,10 @@ function takePhoto() {
 
     // Takes the data(pic) out of the canvas
     const data = canvas.toDataURL('image/jpeg');
-    console.log(data);
     const link = document.createElement('a');
     link.href = data;
     link.setAttribute('download', 'handsome');
-    link.textContent = 'Download Image';
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
     strip.insertBefore(link, strip.firstChild);
 }
 
